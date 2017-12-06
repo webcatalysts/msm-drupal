0c1e152de060ce58da817d99582e4a1ef711b6e0
speedytwenty <speedytwenty@gmail.com>
2017-12-06 14:50:48 -0700

    Updates and fixes

--- srv/collectionprocess.js
+++ srv/collectionprocess.js
@@ -8,11 +8,53 @@ var CollectionProcessInstance = function (collection, databaseWrapper, databaseP
 }
 
 var ProcessCollection = function (instance, callback) {
-    if (instance.collection.type) {
+    var result = {};
+    switch (instance.collection.type) {
+        case 'mapReduce':
+            var handler = ProcessMapReduce;
+            break;
+        case 'aggregation':
+            var handler = ProcessAggregation;
+            break;
+        case 'custom':
+            var handler = ProcessCustom;
+            break;
+        default:
+            var handler = function () { callback(null, { ok: 1 }); }
+            break;
     }
-    else if (instance.collection.schemaImported) {
-        callback(null, { ok: 0 });
+    if (instance.collection.preProcess) {
+        eval('var preProcess = function() { ' + instance.collection.preProcess + '};');
     }
+    if (instance.collection.postProcess) {
+        eval('var postProcess = function() { ' + instance.collection.postProcess + '};');
+    }
+    instance.collectionProvider.findById(instance.collection.source, function (err, source) {
+        instance.source = source;
+        handler(instance, function () {});
+    });
+    callback(null, { ok: 0 });
+}
+
+var ProcessMapReduce = function (instance, callback) {
+}
+
+var ProcessCustom = function (instance, callback) {
+    instance.databaseWrapper.connect(function (err, con) {
+        var sourceDatabaseName = instance.source.database;
+        var sourceDatabase = con.db(sourceDatabaseName);
+        var sourceCollectionName = instance.source.collection;
+        var sourceCollection = sourceDatabase.collection(sourceCollectionName);
+        if (instance.collection.persist) {
+            var destinationDatabaseName = instance.collection.database;
+            var destinationDatabase = con.db(destinationDatabaseName);
+            var destinationCollectionName = instance.collection.collection;
+            var destinationCollection = destinationDatabase.collection(destinationCollectionName);
+        }
+        console.log('eval custom');
+        eval('var evalCustom = function () { ' + instance.collection.eval.code + ' };');
+        evalCustom();
+    });
 }
 
 module.exports = {
--- srv/schemaanalyzer.js
+++ srv/schemaanalyzer.js
@@ -36,82 +36,109 @@ var analyzeSchema = function(schemaAnalysis, options = {}, callback) {
         var spawnArgs = [schemaAnalysis.databaseName, '--eval='+utils.buildParams(schemaAnalysis.collectionName, options), './node_modules/variety/variety.js'];
 
         var promise = child.spawn('mongo', spawnArgs);
-        console.log('spawn started');
+        var childProcess = promise.childProcess;
+        console.log('[spawn] childProcess.pid: ', childProcess.pid);
+        childProcess.stdout.on('data', function (data) {
+            console.log('[spawn] stdout: ', data.toString());
+        });
+        childProcess.stderr.on('data', function (data) {
+            console.log('[spawn] stderr: ', data.toString());
+        });
         return promise;
     }
 
     var extractSchema = function(callback) {
         schemaAnalysis.databaseWrapper.connect(function(err, con) {
             con.db(schemaDatabase).collection(schemaCollection).find().toArray(function(err, fields) {
-                var fieldNames = [];
-                var schema = {};
-                var numFields = fields.length;
-                for (var i = 0; i < numFields; i++) {
-                    var field = fields[i];
-                    if (typeof field.value.types.Number !== 'undefined' || typeof field.value.types.NumberLong !== 'undefined') {
-                        var value = { type: 'int' };
-                    }
-                    else if (typeof field.value.types.String !== 'undefined') {
-                        var value = { type: 'text' };
-                    }
-                    else if (typeof field.value.types.Object !== 'undefined') {
-                        var value = { type: 'group', children: {} };
-                    }
-                    else if (typeof field.value.types.Array !== 'undefined') {
-                        var value = { type: 'array', children: {} };
-                    }
-                    else if (typeof field.value.types.Boolean !== 'undefined') {
-                        var value = { type: 'boolean' };
-                    }
-                    else { continue; }
+                if (err) { console.log(err); callback(err); }
+                else {
+                    var fieldNames = [];
+                    var schema = {};
+                    var numFields = fields.length;
+                    for (var i = 0; i < numFields; i++) {
+                        var field = fields[i];
+                        if (typeof field.value.types.Number !== 'undefined' || typeof field.value.types.NumberLong !== 'undefined') {
+                            var value = { type: 'int' };
+                        }
+                        else if (typeof field.value.types.String !== 'undefined') {
+                            var value = { type: 'text' };
+                        }
+                        else if (typeof field.value.types.Object !== 'undefined') {
+                            var value = { type: 'group', children: {} };
+                        }
+                        else if (typeof field.value.types.Array !== 'undefined') {
+                            var value = { type: 'array', children: {} };
+                        }
+                        else if (typeof field.value.types.Boolean !== 'undefined') {
+                            var value = { type: 'boolean' };
+                        }
+                        else { continue; }
 
-                    var fieldName = field['_id'].key;
+                        var fieldName = field['_id'].key;
 
-                    if (fieldName.match(/^_versionHash/) || fieldName.match(/^_changeHistory/)) {
-                        continue;
-                    }
+                        if (fieldName.match(/^_versionHash/) || fieldName.match(/^_changeHistory/)) {
+                            continue;
+                        }
 
-                    if (fieldName.match(new RegExp(/XX\./))) {
-                        var fieldName = fieldName.replace(/XX\./g, "");
+                        if (fieldName.match(new RegExp(/XX\./))) {
+                            var fieldName = fieldName.replace(/XX\./g, "");
+                        }
+                        if (fieldName.match(/\./)) {
+                            var parts = fieldName.split('.');
+                            var lastPart = parts.length-1;
+                            var fieldName = parts[lastPart];
+                            delete parts[lastPart];
+                            var parts = parts.filter(function (part) { return typeof part !== 'undefined' });
+                            var parentFieldName = parts.length > 1 ? parts.join('.children.') : parts[0];
+                            eval('var fieldName = "' + parentFieldName + '.children.' + fieldName + '";');
+                        }
+                        fieldNames.push(fieldName);
+                        eval('schema.' + fieldName + ' = value;');
                     }
-                    if (fieldName.match(/\./)) {
-                        var parts = fieldName.split('.');
-                        var lastPart = parts.length-1;
-                        var fieldName = parts[lastPart];
-                        delete parts[lastPart];
-                        var parts = parts.filter(function (part) { return typeof part !== 'undefined' });
-                        var parentFieldName = parts.length > 1 ? parts.join('.children.') : parts[0];
-                        eval('var fieldName = "' + parentFieldName + '.children.' + fieldName + '";');
-                    }
-                    fieldNames.push(fieldName);
-                    eval('schema.' + fieldName + ' = value;');
+                    callback(null, schema);
                 }
-                callback(null, schema);
             });
         });
     }
 
     schemaAnalysis.collectionProvider.save(schemaAnalysis.collectionId, data, function (err, result) {
-        if (err) callback(err);
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
         else {
             runSchemaAnalysis()
                 .then(function() { 
+                    console.log('schema analyzed');
                     extractSchema(function (err, schema) {
-                        var update = {
-                            "$unset": { analyzingSchema: "" },
-                        }
+                        console.log('extract');
                         if (err) {
-                            update['$set'] = { enabled: false, error: "Failed to import schema." };
+                            console.log(err);
+                            callback(err);
                         }
                         else {
-                            update['$set'] = { enabled: true, schema: schema, schemaImported: true };
+                            var update = {
+                                "$unset": { analyzingSchema: "" },
+                            }
+                            if (err) {
+                                update['$set'] = { enabled: false, error: "Failed to import schema." };
+                            }
+                            else {
+                                update['$set'] = { enabled: true, schema: schema, schemaImported: true };
+                            }
+                            schemaAnalysis.collectionProvider.save(schemaAnalysis.collectionId, update, function(err, res) {
+                                if (err) {
+                                    console.log(err);
+                                    callback(err);
+                                }
+                                else {
+                                    console.log('done with: ' + schemaAnalysis.collectionId);
+                                }
+                            });
                         }
-                        schemaAnalysis.collectionProvider.save(schemaAnalysis.collectionId, update, function(err, res) {
-                            console.log('done with: ' + schemaAnalysis.collectionId);
-                        });
                     });
                 })
-                .catch(function(err) { callback(err); });
+                .catch(function(err) { console.log(err); callback(err); });
 
             callback(null, {ok: 1});
         }
